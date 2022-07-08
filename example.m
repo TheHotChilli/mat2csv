ffd41eae53fcf2f6e09b2747da3fccba581dd41b
TheHotChilli <71588112+TheHotChilli@users.noreply.github.com>
2022-07-08 12:49:07 +0200

    Create Example.m - Initial commit

---
+++ example.m
@@ -0,0 +1,21 @@
+clc
+clear all
+close all
+
+%% Create .mat files
+doubles = sin(0:99);
+text = strings(1,50);
+text(:) = "abcdefg";
+CELL = horzcat(num2cell(doubles(1:35)), num2cell(text(1:35)))';
+chars = convertStringsToChars(text);
+
+save("example1.mat");
+save("example2.mat", "doubles", "CELL");
+
+%% Convert all .mat files to .csv files
+input_dir = pwd;
+files_info = dir(fullfile(input_dir, "*.mat"));                     % Get info of all .mat files in input_dir as struct
+
+for i = 1:length(files_info)                                        % Loop all files in input_dir
+    mat2csv(fullfile(input_dir, files_info(i).name));
+end
